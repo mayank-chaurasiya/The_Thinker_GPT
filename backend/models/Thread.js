81be10efb73fff0c7e3ed4bcb782eecc41a31f94
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-18 02:02:10 +0530

    setting up routes

--- backend/models/Thread.js
+++ backend/models/Thread.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MessageSchema = new mongoose.Schema({
   role: {
     type: String,
-    enum: ["user", "assistand"],
+    enum: ["user", "assistant"],
     required: true,
   },
   content: {
--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Thread from "../models/Thread.js";
+import getGenAIAPIResponse from "../utils/genai.js";
+
 const router = express.Router();
 
 //test
@@ -57,6 +59,45 @@ router.delete("/thread/:threadId", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Failed to delete thread" });
+  }
+});
+
+router.post("/chat", async (req, res) => {
+  const { threadId, message } = req.body;
+
+  if (!threadId || !message) {
+    return res.status(400).json({ error: "missing required fields" });
+  }
+  try {
+    let thread = await Thread.findOne({ threadId });
+    const userMessage =
+      typeof message === "string" ? message : JSON.stringify(message);
+    if (!thread) {
+      //create a new thread in db
+      thread = new Thread({
+        threadId,
+        title: userMessage,
+        messages: [{ role: "user", content: userMessage }],
+      });
+    } else {
+      thread.messages.push({ role: "user", content: userMessage });
+    }
+    const assistantReply = await getGenAIAPIResponse(userMessage);
+
+    thread.messages.push({
+      role: "assistant",
+      content:
+        typeof assistantReply === "string"
+          ? assistantReply.text
+          : JSON.stringify(assistantReply.text),
+    });
+    thread.updatedAt = new Date();
+
+    await thread.save();
+    res.json({ reply: assistantReply.text });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "something went wrong" });
   }
 });
 export default router;
