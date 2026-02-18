bd18775ad085b7c5665ea5c4862f02196773e37b
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-19 02:23:29 +0530

    sidebar setup

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,30 @@
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <section className="sidebar">
+      <button>
+        <img
+          src="src/assets/gemini-icon.png"
+          alt="gpt-logo"
+          className="gemini-logo"
+        />
+        <span>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </span>
+      </button>
+
+      <ul className="history">
+        <li>history1</li>
+        <li>history2</li>
+        <li>history3</li>
+      </ul>
+
+      <div className="sign">
+        <p>By Mayank &hearts;</p>
+      </div>
+    </section>
+  );
 };
 
 export default Sidebar;
